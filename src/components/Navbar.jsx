29c8d8a2d2506f4822e11ac9d46d60990cb7bdb4
Hafiz17 <119962175+RIKI1796@users.noreply.github.com>
2025-04-15 23:12:55 +0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import "./styles/Navbar.css";
 
 function Navbar() {
+  useEffect(() => {
+    const list = document.getElementById("list");
+    const close = document.getElementById("close");
+    const menu = document.getElementById("menu");
+
+    list.onclick = () => {
+      menu.style.transform = "translateX(0)";
+    };
+    close.onclick = () => {
+      menu.style.transform = "translateX(300px)";
+    };
+  });
+
   return (
     <nav>
       <div className="judul">
         <h1>MY PORTOFOLIO</h1>
       </div>
-      <div className="menu">
+      <i id="list" className="bi bi-list"></i>
+      <div id="menu" className="menu">
+        <span id="close">x</span>
         <a href="#about">About</a>
         <a href="#skills">Skills</a>
         <a href="#riwayat">Riwayat-Sekolah</a>
